8766086ea47b7c98feeabcec990ebe3f1f0f6fc4
mann244 <marsaaminullah@gmail.com>
2026-02-15 07:33:12 +0700

    add use head ini app.vue

--- app.vue
+++ app.vue
@@ -7,3 +7,22 @@
   </NuxtLayout>
   </UApp>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-47ZF88NPY4',
+      async: true
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-47ZF88NPY4');
+      `
+    }
+  ]
+})
+</script>
