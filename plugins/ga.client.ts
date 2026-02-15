4dd48a3989596e5aa0dc030df0b306d5ca964c6f
mann244 <marsaaminullah@gmail.com>
2026-02-15 07:27:08 +0700

    update ga.client

--- plugins/ga.client.ts
+++ plugins/ga.client.ts
@@ -4,29 +4,28 @@ export default defineNuxtPlugin(() => {
 
   if (!config.public.gaId) return
 
+  // Inject script
   const script = document.createElement('script')
   script.src = `https://www.googletagmanager.com/gtag/js?id=${config.public.gaId}`
   script.async = true
-
-  script.onload = () => {
-    window.dataLayer = window.dataLayer || []
-
-    function gtag(...args: any[]) {
-      window.dataLayer.push(args)
-    }
-
-    window.gtag = gtag
-
-    window.gtag('js', new Date())
-    window.gtag('config', config.public.gaId)
-
-    // SPA tracking
-    router.afterEach((to) => {
-      window.gtag('config', config.public.gaId, {
-        page_path: to.fullPath,
-      })
-    })
-  }
-
   document.head.appendChild(script)
+
+  // Setup global function (SEBELUM script load)
+  window.dataLayer = window.dataLayer || []
+  function gtag(...args: any[]) {
+    window.dataLayer.push(args)
+  }
+  window.gtag = gtag
+
+  gtag('js', new Date())
+  gtag('config', config.public.gaId, {
+    send_page_view: true
+  })
+
+  // Track SPA navigation
+  router.afterEach((to) => {
+    window.gtag('config', config.public.gaId, {
+      page_path: to.fullPath,
+    })
+  })
 })
